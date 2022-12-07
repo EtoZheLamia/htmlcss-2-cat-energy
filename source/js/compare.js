afea8b6f8d9b93feda4baadf4ce41cb7e327be77
EtoZheLamia <Andariel2175@yandex.ru>
2022-12-07 19:43:22 +0300

    адаптивные изображения на главной странице. Анимация слайдера

---
+++ source/js/compare.js
@@ -0,0 +1,24 @@
+
+
+  const compare = document.querySelector('.example__compare');
+  const btnBefore = compare.querySelector('.example__control-text--before');
+  const btnAfter = compare.querySelector('.example__control-text--after');
+  const imgBefore = compare.querySelector('.example_demo--before');
+  const imgAfter = compare.querySelector('.example_demo--after');
+  const range = compare .querySelector('.example__range');
+
+  range.addEventListener('input', function() {
+    imgBefore.style.width = (100 - range.value) + '%';
+    imgAfter.style.width = range.value + '%';
+  })
+
+  btnBefore.addEventListener('click', function () {
+    imgBefore.style.width = '100%';
+    imgAfter.style.width = '0%';
+    range.value = 0;
+  })
+  btnAfter.addEventListener('click', function () {
+    imgBefore.style.width = '0%';
+    imgAfter.style.width = '100%';
+    range.value = 100;
+  })
