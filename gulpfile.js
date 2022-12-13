9fad787710d9d014acaee79e57d63a1bbaac811f
EtoZheLamia <Andariel2175@yandex.ru>
2022-12-13 13:43:00 +0300

    Переделана сборка в Gulp для 4 версии

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync').create();
 
-// Styles
-
-export const styles = () => {
+//compile scss into css
+const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
-    .pipe(browser.stream());
+  .pipe(plumber())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+  .pipe(browserSync.stream());
+}
+
+// reload
+
+const reload = (done) => {
+  browserSync.reload();
+  done();
 }
 
 // Server
 
-const server = (done) => {
-  browser.init({
+const watch = () => {
+  browserSync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "source",
     },
     cors: true,
     notify: false,
     ui: false,
   });
-  done();
+  gulp.watch('source/sass/**/*.scss', styles)
+  gulp.watch('*source/*.html').on('change',browserSync.reload);
+  gulp.watch('source/js/*.js').on('change', browserSync.reload);
 }
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
-
-
-export default gulp.series(
-  styles, server, watcher
+exports.default = gulp.series(
+  styles, watch,reload
 );
